3b5006ee5a9d9179f4b7418d715f680f67ad8959
Kim Riley <kriley@hkmconsultingllc.com>
2016-03-25 12:11:12 -0400

    Add PUT /todos/:id

--- server.js
+++ server.js
@@ -74,6 +74,38 @@ app.delete('/todos/:id', function(req, res) {
 	}
 });
 
+//PUT /todos/:id
+app.put('/todos/:id', function (req, res) {
+		var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+	var body = _.pick(req.body, 'description', 'completed'); 
+	var validAttributes = {};
+
+	if (!matchedTodo) {
+		return res.status(404).send();
+	}
+
+	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+		validAttributes.completed = body.completed;
+	} else if (body.hasOwnProperty('completed')) {
+		// has completed property but it is not bool
+		return res.status(400).send();
+	} else {
+		// Never provided attribute, no problem here
+	}
+
+	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+		validAttributes.description = body.description;
+	} else if (body.hasOwnProperty('description')) {
+		// has completed property but it is not String or it is empty
+		return res.status(400).send();
+	} 
+
+	_.extend(matchedTodo, validAttributes);
+
+	res.json(matchedTodo);
+
+});
 
 app.listen(PORT, function () {
 	console.log('Express listening on port ' + PORT + '!');
