435ea8c37ca80f8d78b488645541f42412251731
Kim Riley <kriley@hkmconsultingllc.com>
2016-03-30 10:23:06 -0400

    Add todo model and POST /todos

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var db = require('./db.js');
+
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -68,20 +70,31 @@ app.get('/todos/:id', function(req, res) {
 app.post('/todos', function(req, res) {
 	var body = _.pick(req.body, 'description', 'completed'); // use _.pick to only pick description and completed
 
-	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-		return res.status(400).send();
-	}
+	// db.todo.create
+	// 		if successful respond to the API caller with a 200 and the value of the todo oj (toJSON())
+	//		if failed res.status(400).json(e)
 
-	// set body.description to be trimmed value....
-	body.description = body.description.trim();
+	db.todo.create(body).then(function (todo) {
+		res.json(todo.toJSON());
+	}, function (e) {
+		res.status(400).json(e);
+	});
 
-	// add id field and then increment
-	body.id = todoNextId++;
 
-	// push body onto array
-	todos.push(body);
+	// if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+	// 	return res.status(400).send();
+	// }
 
-	res.json(body);
+	// // set body.description to be trimmed value....
+	// body.description = body.description.trim();
+
+	// // add id field and then increment
+	// body.id = todoNextId++;
+
+	// // push body onto array
+	// todos.push(body);
+
+	// res.json(body);
 });
 
 // DELETE /todos/:id
@@ -136,6 +149,8 @@ app.put('/todos/:id', function(req, res) {
 
 });
 
-app.listen(PORT, function() {
-	console.log('Express listening on port ' + PORT + '!');
+db.sequelize.sync().then(function () {
+	app.listen(PORT, function () {
+		console.log('Express listening on port ' + PORT + '!');
+	});
 });
