71510b2658e99a30864254ae6a1550d5ee7daafe
Kim Riley <kriley@hkmconsultingllc.com>
2016-03-30 14:16:27 -0400

    Hash and salt password

--- server.js
+++ server.js
@@ -126,7 +126,7 @@ app.post('/users', function (req, res) {
 	var body = _.pick(req.body, 'email', 'password'); // use _.pick to only pick email and password
 
 	db.user.create(body).then(function (user) {
-		res.json(user.toJSON());
+		res.json(user.toPublicJSON());
 	}, function (e) {
 		res.status(400).json(e);
 	});
