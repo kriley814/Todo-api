92e550ee56f9f1435f0a3ef2dde2f927532867ae
Kim Riley <kriley@hkmconsultingllc.com>
2016-03-31 12:04:35 -0400

    Added todo/user association

--- playground/basic-sqlite-database.js
+++ playground/basic-sqlite-database.js
@@ -19,52 +19,43 @@ var Todo = sequelize.define('todo', {
 	}
 });
 
+var User = sequelize.define('user', {
+	email: Sequelize.STRING
+});
+
+Todo.belongsTo(User);
+User.hasMany(Todo);
+
 sequelize.sync({
-		// force:  true
-	}).then(function() {
-			console.log('Everything is synced');
+		 // force:  true
+	}).then(function () {
+		console.log('Everything is synced');
 
-			// fetch a todo item by its id id=2....
-			// if find it print to screen using toJSON
-			// if do not print "todo not found"
+// filter to only complete status = true... then flip to false...
 
-			Todo.findById(2).then(function(todo) {
-				if (todo) {
-					console.log(todo.toJSON());
-				} else {
-					console.log('No todo found');
+		User.findById(1).then(function (user) {
+			user.getTodos({
+				where: {
+					completed: false
 				}
-			}).catch(function (e) {
-				console.log(e);
+			}).then(function (todos) {
+				todos.forEach(function (todo) {
+					console.log(todo.toJSON());
+				});
 			});
+		});
+
+		// User.create({
+		// 	email: 'kriley@hkmconsultingllc.com'
+		// }).then(function () {
+		// 	return Todo.create({
+		// 		description: 'Clean yard'
+		// 	});
+		// }).then(function (todo) {
+		// 	User.findById(1).then(function (user) {
+		// 		user.addTodo(todo);
+		// 	});
+		// });
 
 
-				// Todo.create({
-				// 	description: 'Take out trash'
-
-				// }).then(function (todo) {
-				// 	return Todo.create({
-				// 		description: 'Clean office'
-				// 	});
-				// }).then(function() {
-				// 	// return Todo.findById(1)
-				// 	return Todo.findAll({
-				// 		where: {
-				// 			description: {
-				// 				$like: '%Office%'
-				// 			}
-				// 		}
-				// 	});
-				// }).then(function (todos) {
-				// 	if(todos) {
-				// 		todos.forEach(function (todo) {
-				// 			console.log(todo.toJSON());
-				// 		});
-
-				// 	} else {
-				// 		console.log('no todo found');
-				// 	}
-				// }).catch(function (e) {
-				// 	console.log(e);
-				// });
-			});
+});
