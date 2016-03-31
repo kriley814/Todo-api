f9cfa19dca941e9467879eecb893b92ef7516677
Kim Riley <kriley@hkmconsultingllc.com>
2016-03-31 12:59:06 -0400

    Convert CRUD requests to use current user todos only

--- server.js
+++ server.js
@@ -19,7 +19,9 @@ app.get('/', function(req, res) {
 // GET /todos?completed=false&q=work
 app.get('/todos', middleware.requireAuthentication, function(req, res) {
 	var query = req.query;
-	var where = {};
+	var where = {
+		userId: req.user.get('id')
+	};
 
 	if (query.hasOwnProperty('completed') && query.completed === 'true') {
 		where.completed = true;
@@ -36,7 +38,7 @@ app.get('/todos', middleware.requireAuthentication, function(req, res) {
 
 	db.todo.findAll({
 		where: where
-	}).then(function(todos) {
+	}).then(function (todos) {
 		res.json(todos);
 	}, function(e) {
 		res.status(500).send();
@@ -48,13 +50,18 @@ app.get('/todos', middleware.requireAuthentication, function(req, res) {
 app.get('/todos/:id', middleware.requireAuthentication, function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
 
-	db.todo.findById(todoId).then(function(todo) {
+	db.todo.findOne({
+		where: {
+			id: todoId,
+			userId: req.user.get('id')		
+		}
+	}).then(function (todo) { 
 		if (!!todo) {
 			res.json(todo.toJSON());
 		} else {
 			res.status(404).send();
 		}
-	}, function(e) {
+	}, function (e) {
 		res.status(500).send();
 	});
 });
@@ -63,7 +70,7 @@ app.get('/todos/:id', middleware.requireAuthentication, function(req, res) {
 app.post('/todos', middleware.requireAuthentication, function(req, res) {
 	var body = _.pick(req.body, 'description', 'completed'); // use _.pick to only pick description and completed
 
-	db.todo.create(body).then(function(todo) {
+	db.todo.create(body).then(function (todo) {
 		//res.json(todo.toJSON());
 		req.user.addTodo(todo).then(function () {
 			return todo.reload();
@@ -82,7 +89,8 @@ app.delete('/todos/:id', middleware.requireAuthentication, function(req, res) {
 
 	db.todo.destroy({
 		where: {
-			id: todoId
+			id: todoId,
+			userId: req.user.get('id')
 		}
 	}).then(function(rowsDeleted) {
 		if (rowsDeleted === 0) {
@@ -111,7 +119,12 @@ app.put('/todos/:id', middleware.requireAuthentication, function(req, res) {
 		attributes.description = body.description;
 	}
 
-	db.todo.findById(todoId).then(function(todo) {
+	db.todo.findOne({
+		where: {
+			id: todoId,
+			userId: req.user.get('id')
+		}
+	}).then(function(todo) {
 		if (todo) {
 			todo.update(attributes).then(function(todo) { // if todo.update succeeds
 				res.json(todo.toJSON());
@@ -128,22 +141,22 @@ app.put('/todos/:id', middleware.requireAuthentication, function(req, res) {
 });
 
 //POST /users
-app.post('/users', function (req, res) {
+app.post('/users', function(req, res) {
 	var body = _.pick(req.body, 'email', 'password'); // use _.pick to only pick email and password
 
-	db.user.create(body).then(function (user) {
+	db.user.create(body).then(function(user) {
 		res.json(user.toPublicJSON());
-	}, function (e) {
+	}, function(e) {
 		res.status(400).json(e);
 	});
 
 });
 
 // POST /users/login
-app.post('/users/login', function (req, res) {
+app.post('/users/login', function(req, res) {
 	var body = _.pick(req.body, 'email', 'password'); // use _.pick to only pick email and password
 
-	db.user.authenticate(body).then(function (user) {
+	db.user.authenticate(body).then(function(user) {
 		var token = user.generateToken('authentication');
 
 		if (token) {
@@ -151,13 +164,15 @@ app.post('/users/login', function (req, res) {
 		} else {
 			res.status(401).send();
 		}
-	}, function () {
+	}, function() {
 		res.status(401).send();
 	});
 
 });
 
-db.sequelize.sync({force: true}).then(function() {
+db.sequelize.sync({
+	force: true
+}).then(function() {
 	app.listen(PORT, function() {
 		console.log('Express listening on port ' + PORT + '!');
 	});
